f4e66c73476c87649aca11aafcd3793fa881779f
Joe Binns <joebinns.95@gmail.com>
2023-03-01 16:14:32 +0100

    Clean up index.html and n-body-simulator.html, as templates

---
+++ src/DocumentDarkModeHome.js
@@ -0,0 +1,10 @@
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (userPrefersDark) {
+    // Swap document icons to white versions to retain contrast
+    document.getElementById("shortcuticon").href="../images/globe_box_optimised_white.svg";
+    document.getElementById("github").src="../images/github_box_optimised_white.svg";
+    document.getElementById("mail").src="../images/mail_box_optimised_white.svg";
+    document.getElementById("itchdotio").src="../images/itchdotio_box_optimised_white.svg";
+    document.getElementById("youtube").src="../images/youtube_box_optimised_white.svg";
+    document.getElementById("linkedin").src="../images/linkedin_box_optimised_white.svg";
+}
