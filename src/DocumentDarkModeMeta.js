67b98fb991a7025c0705943863e2666b30f6ecdd
Joe Binns <joebinns.95@gmail.com>
2023-03-03 11:22:09 +0100

    Make dark mode scripts components

---
+++ src/DocumentDarkModeMeta.js
@@ -0,0 +1,9 @@
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (userPrefersDark) {
+    // Swap document icons to white versions to retain contrast
+    document.getElementById("websiteProj").src="../images/globe_box_optimised_white.svg";
+    document.getElementById("itchdotioProj").src="../images/itchdotio_box_optimised_white.svg";
+    document.getElementById("githubProj").src="../images/github_box_optimised_white.svg";
+    document.getElementById("youtubeProj").src="../images/youtube_box_optimised_white.svg";
+    document.getElementById("documentProj").src="../images/document_box_white.svg";
+}
--- src/DocumentDarkModePage.js
+++ src/DocumentDarkModePage.js
@@ -1,18 +1,4 @@
 const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 if (userPrefersDark) {
-    // Swap document icons to white versions to retain contrast
-    document.getElementById("shortcuticon").href="../images/globe_box_optimised_white.svg";
-
-    document.getElementById("github").src="../images/github_box_optimised_white.svg";
-    document.getElementById("mail").src="../images/mail_box_optimised_white.svg";
-    document.getElementById("itchdotio").src="../images/itchdotio_box_optimised_white.svg";
-    document.getElementById("youtube").src="../images/youtube_box_optimised_white.svg";
-    document.getElementById("linkedin").src="../images/linkedin_box_optimised_white.svg";
     document.getElementById("back").src="../images/back_white.svg";
-
-    document.getElementById("websiteProj").src="../images/globe_box_optimised_white.svg";
-    document.getElementById("itchdotioProj").src="../images/itchdotio_box_optimised_white.svg";
-    document.getElementById("githubProj").src="../images/github_box_optimised_white.svg";
-    document.getElementById("youtubeProj").src="../images/youtube_box_optimised_white.svg";
-    document.getElementById("documentProj").src="../images/document_box_white.svg";
 }
