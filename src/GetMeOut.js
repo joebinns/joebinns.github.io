fd58ba379a74c8e00629cb3b50ad5ade621548ee
Joe Binns <joebinns.95@gmail.com>
2023-03-25 18:24:21 +0100

    Add minimal get-me-out.html

--- src/GetMeOut.js
+++ src/GetMeOut.js
@@ -8,13 +8,8 @@ import { ModelPreviewer } from '../src/ModelPreviewer.js';
 let portfolioItems = [];
 
 portfolioItems.push(
-    new PortfolioItem('getMeOut', 'question-mark-block.glb'),
+    new PortfolioItem(null, 'question-mark-block.glb'),
     new PortfolioItem('worms', 'crown.glb'),
-    new PortfolioItem('ann', 'robot.glb'),
-    new PortfolioItem('scc', 'question-mark-block.glb'),
-    new PortfolioItem('pde', 'painting.glb'),
-    new PortfolioItem('mLabs', 'arc-de-triomphe.glb'),
-    new PortfolioItem('nBody', 'space-shuttle.glb')
 );
 
 let promises = [];
