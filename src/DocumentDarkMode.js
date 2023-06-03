259e6025bf327470b25c740cdcf810e4e70cbd97
Joe Binns <joebinns.95@gmail.com>
2023-06-03 14:36:06 +0200

    Update webpage icon to be initials

--- src/DocumentDarkMode.js
+++ src/DocumentDarkMode.js
@@ -1,7 +1,7 @@
 const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 if (true) {
     // Swap document icons to white versions to retain contrast
-    document.getElementById("shortcuticon").href="/images/globe_box_optimised_white.svg";
+    document.getElementById("shortcuticon").href="/images/jb_white.svg";
 
     document.getElementById("github").src="/images/github_box_optimised_white.svg";
     document.getElementById("mail").src="/images/mail_box_optimised_white.svg";
