adaba19597f78ce151ed7c1acc26526ac76459bb
Joe Binns <joebinns.95@gmail.com>
2023-06-27 23:09:17 +0200

    Add little cosmos page

--- src/Index.js
+++ src/Index.js
@@ -16,7 +16,8 @@ portfolioItems.push(
     new PortfolioItem('mLabs', 'arc-de-triomphe.glb'),
     new PortfolioItem('nBody', 'space-shuttle.glb'),
     new PortfolioItem('wordle', 'alphabet-block.glb'),
-    new PortfolioItem('njuma', 'question-mark-block.glb')
+    new PortfolioItem('njuma', 'question-mark-block.glb'),
+    new PortfolioItem('littleCosmos', 'question-mark-block.glb')
 );
 
 let promises = [];
