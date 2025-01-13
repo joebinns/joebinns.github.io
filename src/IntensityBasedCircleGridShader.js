3b6a494bebc285ca8a9af400ef67b115d5e8848d
joebinns <joebinns.95@gmail.com>
2025-01-13 21:41:52 +0100

    Only shrink radius on press

--- src/IntensityBasedCircleGridShader.js
+++ src/IntensityBasedCircleGridShader.js
@@ -72,7 +72,8 @@ const IntensityBasedCircleGridShader = {
 
             // Mouse
             float blur = 50.0 / iResolution.y;   
-            float r = 0.08 + 0.01 * cos(PI * iTime);
+            float progress = iTime / 0.3;
+            float r = 0.08 - 0.03 * min(progress * progress, 1.0);
             float col = smoothstep( r+blur, r-blur, length( mc - ouv )); 
 
             float mask = 0.0;
