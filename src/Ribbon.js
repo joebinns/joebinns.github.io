a84f44887ce0db78ba695db4320275abc65498f8
joebinns <joebinns.95@gmail.com>
2026-01-15 14:58:50 +0100

    Add ribbon

---
+++ src/Ribbon.js
@@ -0,0 +1,18 @@
+const ribbon = document.getElementById("ribbon");
+
+if (ribbon)
+{
+    const values = [
+        "Hire Me! 🚀",
+        "Open to Work 👀💼",
+        "Recruiters Welcome! 🤝",
+        "Looking for my Next Role 🔍",
+        "Let's Work Together! 😃😌",
+        "Opportunities Encouraged! 🪜",
+        "Now Accepting Cool Jobs 📡"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * values.length);
+    ribbon.textContent = values[randomIndex];
+}
+
