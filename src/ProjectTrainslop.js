abf3af418ae6150393e9f9cfff37a5084b973652
joebinns <joebinns.95@gmail.com>
2026-03-23 21:11:11 +0100

    Add trainslop video to main page

---
+++ src/ProjectTrainslop.js
@@ -0,0 +1,21 @@
+// Portfolio item
+import { PortfolioItem } from './PortfolioItem.js';
+
+// Model previewer
+import { ModelPreviewer } from './ModelPreviewer.js';
+
+
+let portfolioItems = [];
+
+portfolioItems.push(
+    new PortfolioItem(null, 'barricade.glb'),
+    new PortfolioItem('crossings', 'skull.glb'),
+);
+
+let promises = [];
+portfolioItems.forEach(item => {
+    promises.push(item.promise)
+});
+Promise.all(promises).then(() => {
+    new ModelPreviewer(portfolioItems);
+});
