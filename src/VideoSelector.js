9712dfe76e5ffeaf91bade725516f3674a8ba18e
joebinns <joebinns.95@gmail.com>
2026-07-08 13:08:44 +0200

    Revert "Update video selector logic"
    This reverts commit cea36584811c5315e332610ffa522f4060c80405.

--- src/VideoSelector.js
+++ src/VideoSelector.js
@@ -27,69 +27,44 @@ export class VideoSelector {
         requestAnimationFrame(()=>this.Update()); // Only update when tab open
 
         // Update time
-        const previousTime = this.time;
+        let previousTime = this.time;
         this.time = performance.now() / 1000;
-        const deltaTime = this.time - previousTime;
+        let deltaTime = this.time - previousTime;
 
-        // Find the currently hovered video (if any)
-        let hoveredItem = null;
-
-        for (const item of this.videoItems) {
-            if (isElementHovered(item.triggerElement)) {
-                hoveredItem = item;
-                break;
+        // Update trigger elements
+        this.videoItems.forEach(item => {
+            if (isElementHovered(item.triggerElement)) {          
+                if (item != this.videoItemToFadeIn) {
+                    this.videoItemsToFadeOut.push(this.videoItemToFadeIn);
+                }
+                this.videoItemToFadeIn = item;
             }
-        }
+        });
 
-        // Switch active video only if a different one is hovered
-        if (hoveredItem && hoveredItem !== this.videoItemToFadeIn) {
+        this.videoItemToFadeIn.on += this.rate * deltaTime;        
+        this.videoItemToFadeIn.on = Math.min(this.videoItemToFadeIn.on, 1.0);
 
-            // Fade out the previous active video (only once)
-            if (
-                this.videoItemToFadeIn &&
-                !this.videoItemsToFadeOut.includes(this.videoItemToFadeIn)
-            ) {
-                this.videoItemsToFadeOut.push(this.videoItemToFadeIn);
-            }
+        for (let i = 0; i < this.videoItemsToFadeOut.length; i++) {
+            this.videoItemsToFadeOut[i].on -= this.rate * deltaTime;
+            this.videoItemsToFadeOut[i].on = Math.max(this.videoItemsToFadeOut[i].on, 0.0);
 
-            // Ensure the new active video isn't also fading out
-            this.videoItemsToFadeOut = this.videoItemsToFadeOut.filter(
-                item => item !== hoveredItem
-            );
-
-            this.videoItemToFadeIn = hoveredItem;
-        }
-
-        // Fade in the active video
-        if (this.videoItemToFadeIn) {
-            this.videoItemToFadeIn.on += this.rate * deltaTime;
-            this.videoItemToFadeIn.on = Math.min(this.videoItemToFadeIn.on, 1);
-        }
-
-        // Fade out inactive videos
-        for (let i = this.videoItemsToFadeOut.length - 1; i >= 0; i--) {
-            const item = this.videoItemsToFadeOut[i];
-
-            item.on -= this.rate * deltaTime;
-            item.on = Math.max(item.on, 0);
-
-            if (item.on === 0) {
-                this.videoItemsToFadeOut.splice(i, 1);
+            if (this.videoItemsToFadeOut[i].on <= 0.0) {   
+                this.videoItemsToFadeOut.splice(i, 1);             
             }
         }
 
         // Update overlay
         let maxOn = 0;
-        for (const item of this.videoItems) {
+        this.videoItems.forEach(item => {
             maxOn = Math.max(maxOn, item.on);
-        }
-
-        this.overlay.style.setProperty('--blur', `${32 * (1 - maxOn)}px`);
+        });
+        this.overlay.style.setProperty('--blur', 32 * (1 - maxOn) + 'px');
 
         // Update video elements
-        for (const item of this.videoItems) {
+        this.videoItems.forEach(item => {
             item.videoElement.style.opacity = (item.on * this.maxOpacity).toString();
-            item.videoElement.hidden = item.on <= 0;
-        }
+            item.videoElement.hidden = (item.on <= 0);
+        });
+
     }
 }
